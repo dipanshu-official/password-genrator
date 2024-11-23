f6b4f3a1def52a4c5bfc4ebd9c446dfcf03a0e01
shiva kumar <shivakumar@shivas-MacBook-Air.local>
2024-11-23 19:01:09 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,7 @@ function App() {
     <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3
      my-8 bg-gray-800 text-orange-500'>
 
-      <h1 className='text-white text-center my-3 font-bold'>Password generator by shiva </h1>
+      <h1 className='text-white text-center my-3 font-bold'>Password generator nothing </h1>
 
       <div className='flex shadow rounded-lg overflow-hidden mb-4 '>
         <input 
